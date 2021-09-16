d3e4cd66ce83ad78c668ad70f6789e9cfc6d3dee
zuoxinyu <iamzuoxinyu@gmail.com>
2021-09-17 03:31:48 +0800

    init project

---
+++ Deskband.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows;
+using CSDeskBand;
+using CSDeskBand.ContextMenu;
+
+namespace GlobalMediaControl
+{
+    [ComVisible(true)]
+    [Guid("515F2CAA-73E3-41C3-B224-178DCC7CCA4A")]
+    [CSDeskBandRegistration(Name = "GlobalMediaControl")]
+    public class Deskband : CSDeskBandWpf
+    {
+        public Deskband()
+        {
+            Options.ContextMenuItems = ContextMenuItems;
+        }
+
+        protected override UIElement UIElement => new GolobalMediaControl();
+
+        private List<DeskBandMenuItem> ContextMenuItems
+        {
+            get
+            {
+                var action = new DeskBandMenuAction("Night Mode");
+                action.Clicked += Action_Clicked;
+                return new List<DeskBandMenuItem>() { action, };
+            }
+        }
+
+        private void Action_Clicked(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.NightMode = !Properties.Settings.Default.NightMode;
+        }
+    }
+}
